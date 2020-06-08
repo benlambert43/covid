451ecc6c427516ec740877fcf189e9fe3a5f23a2
benlambert43 <benlambert43@gmail.com>
2020-06-08 09:59:26 -0400

    all

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import MyNav from "./components/MyNav";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  //UncontrolledDropdown,
+  //DropdownToggle,
+  //DropdownMenu,
+  //DropdownItem,
+  NavbarText,
+} from "reactstrap";
 import About from "./components/About";
 import PerState from "./components/PerState";
 import Home from "./components/Home";
 import "./App.css";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
   return (
     <Router>
-      <MyNav />
+      <Navbar color="light" light expand="md">
+        <Link to="/covid/" className="navbar-brand">
+          Covid-19
+        </Link>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <Link to="/covid/perstate" className="nav-link">
+                Per State Data
+              </Link>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/covid/about">About</NavLink>
+            </NavItem>
+            {/*
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  More Information
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+             */}
+          </Nav>
+          <NavbarText>Information</NavbarText>
+        </Collapse>
+      </Navbar>
       <Route path="/covid/perstate" component={PerState} />
       <Route path="/covid/about" component={About} />
       <Route path="/covid/" component={Home} />
--- src/components/MyNav.js
+++ src/components/MyNav.js
@@ -19,38 +19,7 @@ const MyNav = (props) => {
 
   const toggle = () => setIsOpen(!isOpen);
 
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/covid/">Covid-19 US Data</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/covid/perstate">Per State Data</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/covid/about">About</NavLink>
-            </NavItem>
-            {/*
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  More Information
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>Option 1</DropdownItem>
-                  <DropdownItem>Option 2</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Reset</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-             */}
-          </Nav>
-          <NavbarText>Information</NavbarText>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default MyNav;

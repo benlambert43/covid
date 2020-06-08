19804329149feb9af438fcab56954888849f4504
benlambert43 <benlambert43@gmail.com>
2020-06-08 00:44:51 -0400

    Updates

---
+++ precache-manifest.c68694dcd9dd1c7ceb60240528ac5934.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52716eee482dcea89253a41d54a87c02",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "1017125f246385d13666",
+    "url": "/covid/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "44942a06251584b142ea",
+    "url": "/covid/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1017125f246385d13666",
+    "url": "/covid/static/js/2.8c786d13.chunk.js"
+  },
+  {
+    "revision": "fbbfe9ed4dc77af342a4a500830ec9a4",
+    "url": "/covid/static/js/2.8c786d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44942a06251584b142ea",
+    "url": "/covid/static/js/main.4ae79ad8.chunk.js"
+  },
+  {
+    "revision": "6fe33d0aeaf876cdae9a",
+    "url": "/covid/static/js/runtime-main.940f64f0.js"
+  }
+]);
